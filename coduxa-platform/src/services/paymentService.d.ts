5ba861db9e1b239cf8c41a9539c88481c9d0d979
Jayem09 <johndinglasan12@gmail.com>
2025-09-05 00:02:03 +0800

    Remove duplicate nested coduxa-platform directory

--- coduxa-platform/src/services/paymentService.d.ts
+++
@@ -1,8 +0,0 @@
-declare const paymentService: {
-  createInvoice(args: { userId: string; amount: number; credits: number; packTitle: string }): Promise<any>;
-  getPackages(): Promise<any>;
-  getUserCredits(userId: string): Promise<any>;
-};
-export default paymentService;
-
-
